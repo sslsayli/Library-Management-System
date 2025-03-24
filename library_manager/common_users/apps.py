907e778d96aa822bdae22aab39700a5917c4bb28
Sayali Lomte <71779955+sslsayli@users.noreply.github.com>
2025-03-24 13:34:08 +0530

    Add files via upload

---
+++ library_manager/common_users/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CommonUsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'common_users'
---
+++ library_manager/common_users/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+class AdminUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class AdminUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = AdminUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["full_name"]
+
+    def __str__(self):
+        return self.email
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
